c544a34f322e1ab905d1a587db659af391235a80
Mysterious Ben <datascience@tuta.io>
2021-05-03 07:36:56 +0300

    style: fix flake8 settings

--- xmlrecords/src/xmlrecords.py
+++ xmlrecords/src/xmlrecords.py
@@ -1,5 +1,5 @@
-from dataclasses import dataclass
-from typing import Dict, List, Optional, TypedDict
+# from dataclasses import dataclass
+from typing import Dict, List, Optional
 from xml.etree import ElementTree
 
 
